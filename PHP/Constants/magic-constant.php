403d228868c591cf7f5481105fbb1165ff6f7a88
Anubhav Srivastav <anubhavsrivastavbst@gmail.com>
2026-07-22 23:18:23 +0530

    Add php code examples for operator precedence, error handling, backticks, and constants

---
+++ PHP/Constants/magic-constant.php
@@ -0,0 +1,6 @@
+<?php
+
+echo __LINE__."<br>";
+
+echo __FILE__;
+?>
---
+++ PHP/Namespaces/namespace2.php
@@ -0,0 +1,10 @@
+<?php
+
+include "namespaceConstants.php";
+
+echo  MyConstants\FILE_NAME;
+
+Myconstants\brother\hello();
+
+Myconstants\sister\hello2();
+?>
