5e526e84b62eef5fb94455af6f770f70216cfa79
Anubhav Srivastav <anubhavsrivastavbst@gmail.com>
2026-07-23 22:43:58 +0530

    Add practice scripts for control structures and namespaces

---
+++ PHP/Control structures/if-&-switch.php
@@ -0,0 +1,22 @@
+<?php
+
+$i=1;
+
+switch($i){
+    case 0 :
+        if($i==0){
+            echo "i is zero";
+        }
+        break;
+    case 1:
+        if($i==1){
+            echo "i is one";
+        }
+        break;
+    case 2:
+        if($i>1){
+            echo "i is greater than 1";
+        }
+        break;
+}
+?>
---
+++ PHP/Namespaces/namespace11.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace jammy;
+const NAME = "Hello php";
+
+
+?>
