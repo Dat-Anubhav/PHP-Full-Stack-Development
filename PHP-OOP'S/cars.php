91da8e5626cf369dd4dcfbcc8be7ae5877b8884a
Anubhav Srivastav <anubhavsrivastavbst@gmail.com>
2026-08-08 22:57:25 +0530

    Add PHP OOP practice script for classes and objects

---
+++ PHP-OOP'S/cars.php
@@ -0,0 +1,32 @@
+<?php
+/* 
+1. Write PHP Program to Define Three Car Types and its Color.
+2. Display Car Type and its Color.
+*/
+
+class car
+{
+    public $name;
+    public $color;
+
+    function set_name($name1)
+    {
+        $this->name=$name1;
+    }
+
+    function set_color($color)
+    {
+        $this->color=$color;
+    }
+}
+$cool_car= new car();
+
+$cool_car->set_name('ferrari');
+echo "<br>";
+$cool_car->set_color('red');
+
+echo "cool car is :".$cool_car->name."  "."color is: ".$cool_car->color;
+
+
+
+?>
