dada3e08dd5b7087f3e0e739c301f6580840780a
Anubhav Srivastav <anubhavsrivastavbst@gmail.com>
2026-08-24 16:35:44 +0530

    Add Laravel routing practice script

--- Laravel/workhood/routes/web.php
+++ Laravel/workhood/routes/web.php
@@ -1,7 +1,15 @@
 <?php
-
+use App\Http\Controllers\ClaimController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
+}); 
+
+Route::get('/first', function (){
+    return view('first');
 });
+
+Route::get('/con', [ClaimController::class, 'index']);
+
