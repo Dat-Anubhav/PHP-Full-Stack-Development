71af37fd48069370975f75e8679eba0ec93730c6
Anubhav Srivastav <anubhavsrivastavbst@gmail.com>
2026-08-25 00:08:12 +0530

    Implement parameterized routes, multi-parameter handling, and route-level dependency injection

--- Laravel/workhood/routes/web.php
+++ Laravel/workhood/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\ClaimController;
 use Illuminate\Support\Facades\Route;
+use App\Services\GreetingService;
 
 
 Route::get('/', function () {
@@ -18,5 +19,18 @@ Route::any('/users', function () {
 return 'User Created Yippeee';
 });
 
+//Route Parameters
 
+Route::get('/users/{id}', function (string $id){
+    return "The user is is $id";
+});
 
+//Route with multiple parameters
+Route::get('/users/{id}/comments/{name}', function (string $id, string $name){
+    return "The user is is $id and the comment is $name";
+});
+
+//Route with Dependency Injection
+Route::get('/dep/{name}', function (string $name, GreetingService $service, ){
+    return $service->greet($name);
+});
